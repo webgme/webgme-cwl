6f3d594499ceaad395e172673eae6c43f209e80c
kecso <kecsoloopy@gmail.com>
2023-04-10 11:32:06 -0500

    WIP eventing
    trying to get connection start event to work

--- src/visualizers/widgets/CommonWorkflowEditor/src/mainFlow.jsx
+++ src/visualizers/widgets/CommonWorkflowEditor/src/mainFlow.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import ReactFlow, { useNodesState, useEdgesState, addEdge } from 'reactflow';
 import 'reactflow/dist/style.css';
 
@@ -11,7 +11,7 @@ import PortNode from './portnode';
 const nodeTypes = {'port': PortNode}
 
 const initialNodes = [
-  { id: '1', type: 'port', position: { x: 100, y: 100 }},
+  { id: '1', type: 'port', position: { x: 100, y: 100 }, data:{toolbarVisible:true}},
   { id: '2', position: { x: 200, y: 200 }, data: { label: '2' } },
 ];
 // const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }];
@@ -21,7 +21,30 @@ export default function Flow(props) {
   const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
   const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
 
-  const onConnect = useCallback((params) => setEdges((eds) => addEdge(params, eds)), [setEdges]);
+  //const onConnect = useCallback((params) => setEdges((eds) => addEdge(params, eds)), [setEdges]);
+
+  const onConnect = useCallback(params=>{
+    setEdges(eds=>addEdge(params,eds));
+    setNodes(nds=>nds.map(node=>{
+      if(node.id === '2') {
+        node.data.label += '+';
+      }
+      return node;
+    }));
+  },[setEdges, setNodes]);
+  const onConnectStart = useCallback((event, params) => {
+    console.log('OCS', params);
+    if(params.nodeId === '1') {
+      console.log('OCS-in');
+      setNodes((nds)=>nds.map((node)=>{
+        if(node.id === '1') {
+          console.log('OCS-set');
+          node.data.toolbarVisible = !node.data.toolbarVisible;
+        }
+        return node;
+      }));
+    }
+  },[setNodes]);
 
   return (
     <div style={{ width: '100vw', height: '100vh' }}>
@@ -31,6 +54,7 @@ export default function Flow(props) {
         onNodesChange={onNodesChange}
         onEdgesChange={onEdgesChange}
         onConnect={onConnect}
+        onConnectStart={onConnectStart}
         nodeTypes={nodeTypes}
         fitView
       >
