591a1a7ab59f5dd1ac98313a2e356812be005806
kecso <kecsoloopy@gmail.com>
2023-04-10 11:32:06 -0500

    WIP eventing
    trying to get connection start event to work

--- src/visualizers/widgets/CommonWorkflowEditor/src/portNode.jsx
+++ src/visualizers/widgets/CommonWorkflowEditor/src/portNode.jsx
@@ -1,21 +1,24 @@
 import { useContext } from 'react';
-import {Handle, Position} from 'reactflow';
+import {Handle, Position, NodeToolbar} from 'reactflow';
 import IconButton from '@mui/material/IconButton';
 import FloatingLabel from 'react-bootstrap/FloatingLabel';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { icon } from '@fortawesome/fontawesome-svg-core/import.macro';
 import Tooltip from '@mui/material/Tooltip';
 
-export default function PortNode(props) {
+export default function PortNode({data}) {
+    // console.log(data);
     return (
         <>
-        <div className='react-flow__node-default'>
-        <FloatingLabel label="something"/>
+        <NodeToolbar isVisible={data.toolbarVisible}>
         <Tooltip title='configure'>
             <IconButton>
                 <FontAwesomeIcon icon={icon({name: 'cog'})} size="sm"/>
             </IconButton>
         </Tooltip>
+        </NodeToolbar>
+        <div className='react-flow__node-default'>
+        <FloatingLabel label="something"/>
         </div>
         <Tooltip title='myhandle'>
             <Handle type="source" position={Position.Bottom} id="a" />
