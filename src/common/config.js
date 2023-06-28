4bf6a8cdc4251eab1803c6550cbaced44aa40891
Tamas Kecskes <tamas.kecskes@vanderbilt.edu>
2023-06-27 23:44:08 -0500

    WIP - last minute fixes before release
    added opendashboard plugin for good measure
    fixes to the linking in the taxonomy during release

---
+++ src/common/config.js
@@ -0,0 +1,41 @@
+define(['text!./pluginconf.json'], function (CONF) {
+
+    let everyConfig = {};
+    try {
+        everyConfig = JSON.parse(CONF);
+    } catch {
+        //TODO
+    }
+
+    const getMyConfig = function(pluginName) {
+        const resultConfig = {};
+        Object.keys(everyConfig.common || {}).forEach(key => {
+            resultConfig[key] = everyConfig[key];
+        });
+
+        Object.keys(everyConfig[pluginName] || {}).forEach(key => {
+            resultConfig[key] = everyConfig[pluginName][key];
+        });
+
+        return resultConfig;
+    };
+
+    const getDashboardUrl = () => {
+
+        try {
+            let url = everyConfig.common.taxonomy.url + '/routers/Dashboard/';
+            url += encodeURIComponent(everyConfig.common.taxonomy.id);
+            url += '/branch/master/static/index.html';
+
+            return url;
+        } catch {
+            return 'https://google.com';
+        }
+    };
+
+    return {
+        getMyConfig: getMyConfig,
+        getDashboardUrl: getDashboardUrl
+
+    };
+});
