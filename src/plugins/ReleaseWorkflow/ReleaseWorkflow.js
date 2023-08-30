45664f263d46b5026cf341e3e94e3019161622e1
kecso <kecsoloopy@gmail.com>
2023-08-30 11:00:16 -0500

    WIP - fix of tagging

--- src/plugins/ReleaseWorkflow/ReleaseWorkflow.js
+++ src/plugins/ReleaseWorkflow/ReleaseWorkflow.js
@@ -268,7 +268,7 @@
         })
         .then(jsonResponse => {
             console.log('JSONRESPONSE:', jsonResponse);
-            nextIndex = jsonResponse.numObservations || 'errortag';
+            nextIndex = '_rel_' + jsonResponse.numObservations;
 
             core.setAttribute(activeNode, 'previousUri', core.getAttribute(activeNode, 'uri') || 'none');
             core.setAttribute(activeNode, 'uri', composeUri());
